a88ba3250ae05e8d115d25b12bcccd7dc04bfdc2
Naufal Akbar <kbrnugroho@gmail.com>
2022-01-20 10:28:10 +0700

    refactor

--- cashier/register-customer.php
+++ cashier/register-customer.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Registrasi Pelanggan | UKL Laundry</title>
+    <title>Dashboard Kasir | UKL Laundry</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Naufal Akbar Nugroho">
@@ -47,8 +47,8 @@
                         </div>
                         <div class="divide-y divide-gray-200">
                             <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                                <form action="utils/process-register-customer.php" method="post">
-                                    <div class="relative">
+                                <form action="../utils/process-register-customer.php" method="post">
+                                    <div class="relative mt-5">
                                         <input autocomplete="off" id="name" name="name" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none" placeholder="Name" />
                                         <label for="name" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Name</label>
                                     </div>
